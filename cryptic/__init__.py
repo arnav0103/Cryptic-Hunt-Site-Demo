8275a49c9b9b125af86b1b6625580f33dc43c2e3
arnav0103 <arnav.gupta0103@gmail.com>
2022-01-05 13:49:00 +0530

    a

--- cryptic/__init__.py
+++ cryptic/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_pymongo import PyMongo
+import dns
 
 login_manager = LoginManager()
 app =  Flask(__name__)
